e8fe01d001b6758b946b07959c997a663886f2fb
doit-zero <131241458+doit-zero@users.noreply.github.com>
2024-07-23 20:15:32 +0900

    자바 volatile 적용
    - volatile을 활용해 메모리 가시성 해결

--- src/thread/volatile1/VolatileCountMain.java
+++ src/thread/volatile1/VolatileCountMain.java
@@ -2,36 +2,31 @@ package thread.volatile1;
 
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
-
 public class VolatileCountMain {
 
     public static void main(String[] args) {
         MyTask task = new MyTask();
-        Thread t = new Thread(task, "work");
+        Thread t = new Thread(task,"work");
         t.start();
 
         sleep(1000);
-
         task.flag = false;
-        log("flag = " + task.flag + ", count = " + task.count + " in main");
+        log("flag = " + task.flag + ", count = " + task.count + "in main");
+
     }
 
-    static class MyTask implements Runnable {
-        //boolean flag = true;
-        //long count;
+    static class MyTask implements Runnable{
         volatile boolean flag = true;
-        volatile long count;
+        long count;
 
         @Override
         public void run() {
-            while(flag) {
+            while (flag){
                 count++;
-                //1억번에 한번씩 출력
-                if (count % 100_000_000 == 0) {
-                    log("flag = " + flag + ", count = " + count + " in while()");
+                if(count % 100_000_000 == 0){
+                    log("flag = " + flag + ", count = " + count + "in while()");
                 }
-            }
-            log("flag = " + flag + ", count = " + count + " 종료");
+            } log("flag = " + flag +", count = "+ count+ "종료");
         }
     }
 }
